d83119411b47012119f1f45756e780ed927ee06c
panyy <panyy@wtvdev.com>
2018-05-14 21:29:43 +0800

    add project

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # grpcglb
 grpcglb, a example for learn grpc glb structure
+
+ReadMe form https://github.com/wwcd/grpc-lb
+
+# 说明
+
+[gRPC服务发现&负载均衡](https://segmentfault.com/a/1190000008672912)中的例子, 修订如下问题
+
+- register中重复PUT, watch时没有释放导致的内存泄漏
+- 退出时不能正常unregister
+
+## 启动测试程序
+
+    # 分别启动服务端
+    go run cmd/svr/svr.go - port 50001
+    go run cmd/svr/svr.go - port 50002
+    go run cmd/svr/svr.go - port 50003
+
+    # 启动客户端
+    go run cmd/cli/cli.go
+	
---
+++ cmd/cli/cli.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"strconv"
+	"time"
+
+	"google.golang.org/grpc"
+
+	etcdnaming "github.com/coreos/etcd/clientv3/naming"
+
+	pb "github.com/panyingyun/grpcglb/cmd/helloworld"
+	grpclb "github.com/panyingyun/grpcglb/etcdv3"
+)
+
+var (
+	serv = flag.String("service", "hello_service", "service name")
+	reg  = flag.String("reg", "http://123.206.185.178:2379", "register etcd address")
+)
+
+func main() {
+	flag.Parse()
+	r := grpclb.NewResolver(*serv)
+
+	b := grpc.RoundRobin(r)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	conn, err := grpc.DialContext(ctx, *reg, grpc.WithInsecure(), grpc.WithBalancer(b), grpc.WithBlock())
+	cancel()
+	if err != nil {
+		panic(err)
+	}
+
+	ticker := time.NewTicker(time.Second)
+	for t := range ticker.C {
+		client := pb.NewGreeterClient(conn)
+		resp, err := client.SayHello(context.Background(), &pb.HelloRequest{Name: "world " + strconv.Itoa(t.Second())})
+		if err == nil {
+			fmt.Printf("%v: Reply is %s\n", t.Local(), resp.Message)
+		}
+	}
+}
